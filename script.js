732af1d69576df678af8e96104143cfe2cca412e
SCxoneZ <stevenniuschandra@gmail.com>
2022-01-10 08:04:50 +0700

    beautifying the code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.querySelectorAll('.card')
+function checkAlert(){
+	document.querySelectorAll('.card')
 	.forEach(el => {
-		el.querySelector('.alert-close').addEventListener('click', (e) => e.target.parentElement.remove())
+	el.querySelector('.alert-close').addEventListener('click', (e) => e.target.parentElement.remove())
 	})
+}
